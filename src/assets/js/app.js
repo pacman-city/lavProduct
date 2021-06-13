181813f993ecf743e7b61d38ef293ab62e9dccba
Евгений Клюев <craftycoderpro@gmail.com>
2021-06-13 17:27:46 +0300

    add few sections

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const hamburgerBtn = document.querySelector('.hamburger');
+  const header = document.querySelector('.header');
+  const toggleMenu = function() {
+    this.classList.toggle('is-active');
+    header.classList.toggle('menu-open');
+  }
+
+  hamburgerBtn.addEventListener('click', toggleMenu);
+
 })
